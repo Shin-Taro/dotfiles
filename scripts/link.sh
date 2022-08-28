0bd3bdc7622f15dc87505cbcfc28656fe8e751b6
ShintaroTakahashi <shintaro_takahashi@bita.jp>
2022-08-28 18:27:10 +0900

    fix: skip link process if file name is "." or ".."

--- scripts/link.sh
+++ scripts/link.sh
@@ -5,5 +5,8 @@ dotfiles_root=$(cd $(dirname $0)/.. && pwd)
 # dotfilesディレクトリの中身のリンクをホームディレクトリ直下に作成
 cd ${dotfiles_root}/dotfiles
 for file in .*; do
+  if [ "${file}" = "." ] || [ "${file}" = ".." ]; then
+    continue
+  fi
     ln -s ${PWD}/${file} ${HOME}
 done
