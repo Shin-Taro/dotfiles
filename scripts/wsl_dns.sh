0bd3d1a67f84aeaa81beffe47695636b7db1e348
Shin-Taro <sh.takahashi.113@gmail.com>
2023-08-14 22:33:59 +0900

    feat: fix wsl dns optimization

--- scripts/wsl_dns.sh
+++ scripts/wsl_dns.sh
@@ -2,4 +2,4 @@
 
 # Optimize WSL2 DNS setting
 sudo sh -c "echo 'nameserver 8.8.8.8' >> /etc/resolv.conf"
-sudo sh -c "echo -e '[network]\ngenerateResolvConf = false' >> /etc/resolv.conf"
+sudo sh -c "echo -e '[network]\ngenerateResolvConf = false' >> /etc/wsl.conf"
