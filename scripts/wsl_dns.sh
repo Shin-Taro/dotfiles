df31357733925aa446d17ead711bfcafa3d9eefa
Shin-Taro <sh.takahashi.113@gmail.com>
2023-08-20 19:39:45 +0900

    feat: remove unnecessary -e option

--- scripts/wsl_dns.sh
+++ scripts/wsl_dns.sh
@@ -2,4 +2,4 @@
 
 # Optimize WSL2 DNS setting
 sudo sh -c "echo 'nameserver 8.8.8.8' >> /etc/resolv.conf"
-sudo sh -c "echo -e '[network]\ngenerateResolvConf = false' >> /etc/wsl.conf"
+sudo sh -c "echo '[network]\ngenerateResolvConf = false' >> /etc/wsl.conf"
