b3ea689af3f3d2718f36ef2cda9de2bd5e623c28
Shin-Taro <sh.takahashi.113@gmail.com>
2023-10-01 12:37:34 +0900

    feat: fix link.sh

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-dotfiles_root=$(cd $(dirname -- $0)/.. && pwd)
-
 # dotfilesディレクトリの中身のリンクをホームディレクトリ直下に作成
-cd ${dotfiles_root}/dotfiles
+cd ~/.dotfiles/dotfiles || exit
 for file in .*; do
   if [ "${file}" = "." ] || [ "${file}" = ".." ]; then
     continue
