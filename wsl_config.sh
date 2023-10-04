b68f48864b472fa66743a6066fc4789177a20196
Shin-Taro <sh.takahashi.113@gmail.com>
2023-10-04 23:03:16 +0900

    feat: add wsl_config entry file

---
+++ wsl_config.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sudo sh -c "echo 'nameserver 8.8.8.8' >> /etc/resolv.conf"
+sudo sh -c "echo '[network]\ngenerateResolvConf = false' >> /etc/wsl.conf"
+sudo sh -c "echo '[automount]\noptions = \"metadata\"' >> /etc/wsl.conf"
+
+echo "( ﾉ ﾟｰﾟ)ﾉ The configuration change of WSL2 was successful"
