72ab790eb8197de0e9e559e7b91eb9c994405467
Shin-Taro <sh.takahashi.113@gmail.com>
2023-10-08 00:43:06 +0900

    feat: add chattr for resolv.conf

--- wsl_config.sh
+++ wsl_config.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 sudo sh -c "echo 'nameserver 8.8.8.8' >> /etc/resolv.conf"
+sudo chattr +i /etc/resolv.conf
+
 sudo sh -c "echo '[network]\ngenerateResolvConf = false' >> /etc/wsl.conf"
 sudo sh -c "echo '[automount]\noptions = \"metadata\"' >> /etc/wsl.conf"
 
