5bf1e70c7636d5dfdd08a46a3e105cd242f53e23
Shin-Taro <sh.takahashi.113@gmail.com>
2023-10-08 02:24:36 +0900

    feat: fix chmod ssh key link

--- scripts/modules/link_ssh_key.sh
+++ scripts/modules/link_ssh_key.sh
@@ -5,7 +5,7 @@ read -p "[HOST_USERNAME]:" HOST_USERNAME
 # link Host OS ssh keys
 ln -s /mnt/c/Users/$HOST_USERNAME/.ssh ~/.ssh
 
-chmod 0700 /mnt/c/Users/$HOST_USERNAME/.ssh && chmod 0600 /mnt/c/Users/$HOST_USERNAME/.ssh/*
+chmod 777 /mnt/c/Users/$HOST_USERNAME/.ssh && chmod 660 /mnt/c/Users/$HOST_USERNAME/.ssh/*
 
 sudo chmod 777 ~/.ssh/Shin-Taro
 sudo chmod 600 ~/.ssh/Shin-Taro/github
