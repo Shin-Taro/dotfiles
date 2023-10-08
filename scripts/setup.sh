18d46ae2277030a842061e7b0832f0eba40f9b27
Shin-Taro <sh.takahashi.113@gmail.com>
2023-10-08 20:37:45 +0900

    refactor: rename var

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-CURRENT_DIRECTORY="$(dirname "${BASH_SOURCE[0]}")"
+THIS_DEIRECTORY="$(dirname "${BASH_SOURCE[0]}")"
 
-. "${CURRENT_DIRECTORY}/modules/install.sh"
-. "${CURRENT_DIRECTORY}/modules/link.sh"
+. "${THIS_DEIRECTORY}/modules/install.sh"
+. "${THIS_DEIRECTORY}/modules/link.sh"
 . "${HOME}/.bashrc"
-. "${CURRENT_DIRECTORY}/modules/install_nodejs.sh"
+. "${THIS_DEIRECTORY}/modules/install_nodejs.sh"
 
 echo -e "\n\e[32;1m=============================================\e[m\n
 \e[32;1m( ﾉ ﾟｰﾟ)ﾉ \e[m Complete setup.\n
