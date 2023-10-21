460a94b87997750cf5441c9437cc70e38009d190
Shin-Taro <sh.takahashi.113@gmail.com>
2023-10-21 17:24:56 +0900

    feat: add entry script for native Ubuntu

---
+++ entry_ubuntu.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# git config
+curl -o ~/temp.sh https://raw.githubusercontent.com/Shin-Taro/dotfiles/develop/scripts/modules/git_config.sh && . ~/temp.sh
+rm ~/temp.sh
+
+# dotfilesをclone
+cd "${HOME}" || return
+git clone git@github.com:Shin-Taro/dotfiles.git
+
+# setup
+# shellcheck source=./dotfiles/scripts/setup.sh
+. ./dotfiles/scripts/setup.sh
+
+echo -e "\n\e[32;1m=============================================\e[m\n
+\e[32;1m( •̀ ω •́ )y \e[m Complete all setup process. Congratulations.\n
+\e[32;1m=============================================\e[m\n"
